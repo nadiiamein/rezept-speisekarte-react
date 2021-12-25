8e6b3a1e393f24ba61f94341f570ab47c631cf8a
nadiiamein <npodufala15@gmail.com>
2021-12-25 12:26:59 +0100

    add function to search for the recipes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import RecipeCard from "./components/RecipeCard";
+import RecipeBar from "./components/SearchBar";
+
+const apiUrl = "https://api.edamam.com/api/recipes/v2";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+
+  // function to search for the recipes
+  
+  const searchRecipes = async () => {
+setIsLoading(true);
+const url = apiUrl + query;
+const res = await fetch(url);
+const data = await res.json();
+//console.log(data);
+setRecipes(data.meals);
+setIsLoading(false);
+  };
+
+  useEffect(() => {
+    searchRecipes();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>Unsere Resept-speice-app</h2>
+      <div className="recipes">
+        {recipes
+          ? recipes.map((recipe) => (
+              <RecipeCard key={recipe.idMeal} recipe={recipe} />
+            ))
+          : "No Results."}
+      </div>
     </div>
   );
 }
